092d2189d538e05972407e8870332f7dd85aa8ab
Ghiftee <gigibetine@gmail.com>
2021-07-05 12:07:11 +0100

    Add a function to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let books = [new Book("Book1", "author1"), 
+            new Book("Book2", "Author2"), 
+            new Book("Book3", "Author3"), 
+            new Book("Book4", "Author4")];
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+window.addEventListener('')
