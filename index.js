c4cdfc5856b29253b4ab8bc9a3ac2936836c914a
Akasha <Akasha.Rojee@gmail.com>
2021-07-06 16:25:47 +0400

    Refactor JS code

--- index.js
+++ index.js
@@ -2,10 +2,21 @@
 
 let books = [];
 
-function Book(title, author, id) {
+function Book(id, title, author) {
+  this.id = id;
   this.title = title;
   this.author = author;
-  this.id = id;
+}
+
+// Create HTML element of given type and add classes, attributes and textContent (where applicable)
+function createElement(elementType, classNames = '', attributes = {}, innerHTML = '') {
+  const elementObject = document.createElement(elementType);
+  if (classNames) elementObject.classList.add(...(classNames.split(' ')));
+  Object.keys(attributes).forEach((attribute) => {
+    elementObject.setAttribute(attribute, attributes[attribute]);
+  });
+  elementObject.innerHTML = innerHTML;
+  return elementObject;
 }
 
 // Populate books section dynamically using local storage
@@ -19,22 +30,17 @@ function populateBooks() {
 
     books = [];
 
-    booksLS.forEach((book) => {
-      books.push(book);
+    booksLS.forEach((book, index) => {
+      
+      books.push(new Book(index + 1, book.title, book.author));
 
       const bookContainer = document.createElement('div');
-      const bookTitle = document.createElement('p');
-      bookTitle.innerHTML = book.title;
-      const bookAuthor = document.createElement('p');
-      bookAuthor.innerHTML = book.author;
+      const bookTitle = createElement('p', '', {}, book.title);
+      const bookAuthor = createElement('p', '', {}, book.author);
       const separator = document.createElement('hr');
 
-      const removeButton = document.createElement('button');
-      // removeButton.classList.add('remove-btn');
-      removeButton.innerHTML = 'Remove';
-      removeButton.addEventListener('click', () => {
-        this.removeBook(book.id)
-      });
+      const removeButton = createElement('button', 'remove-btn', {}, 'Remove');
+      removeButton.addEventListener('click', removeBook);
 
       bookContainer.append(bookTitle, bookAuthor, removeButton, separator);
       bookList.append(bookContainer);
@@ -42,39 +48,39 @@ function populateBooks() {
   }
 }
 
-// Add book to collection, update local storage, and refresh displayed list
-function addBook(e) {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const id = Math.random() * 100;
-  books.push(new Book(title, author, id));
+//After adding/removing book from collection, update local storage, and refresh displayed list
+function refreshBooks() {
   localStorage.setItem('books', JSON.stringify(books));
   populateBooks();
-  e.preventDefault();
 }
 
-// Remove book from collection, update local storage, and refresh displayed list
-function removeBook(id) {
-  // const removeButtons = document.querySelectorAll('.remove-btn');
-  // let bookIndex = 0;
-
-  // Identify which book to remove
-  // removeButtons.forEach((removeButton, index) => {
-  //   if (e.target === removeButton) {
-  //     bookIndex = index;
-  //   }
-  // });
+// Add book to collection
+function addBook(e) {
+  books.push(new Book(
+    books.length + 1,
+    document.getElementById('title').value,
+    document.getElementById('author').value));
   
-  const newBooks = books.filter(
-    (b) => ((b.id !== id)),
-  );
-  books = newBooks;
+  refreshBooks();
+}
 
-  localStorage.setItem('books', JSON.stringify(books));
-  populateBooks();
+// Remove book from collection
+function removeBook(e) {
+
+  //Identify which book to remove
+  const removeButtons = document.querySelectorAll('.remove-btn');
+  const bookIndex = Array.prototype.indexOf.call(removeButtons, e.target);
+  
+  // const newBooks = books.filter(
+  //   (b) => ((b.id !== id)),
+  // );
+  // books = newBooks;
+
+  books.splice(bookIndex, 1);
+  refreshBooks();
 }
 
 const addButton = document.getElementById('add-btn');
 addButton.addEventListener('click', addBook);
 
-window.addEventListener('load', populateBooks);
+document.addEventListener('DOMContentLoaded', populateBooks);
